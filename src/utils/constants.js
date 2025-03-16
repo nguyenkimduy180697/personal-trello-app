0f97a2d700b692fc3de65e366c4b4ce763ecce29
CR_Duy_Nguyen <nguyenkimduy.a15@gmail.com>
2025-03-16 23:10:45 +0700

    (...) update code

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,11 @@
-export const API_ROOT = 'https://personal-trello-api.onrender.com'
-// export const API_ROOT = 'http://localhost:8017'
+
+
+let apiRoot = ''
+
+if (process.env.BUILD_MODE === 'DEV') {
+  apiRoot = 'http://localhost:8017'
+}
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://personal-trello-api.onrender.com'
+}
+export const API_ROOT = apiRoot
